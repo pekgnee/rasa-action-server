eac24a59b1ab71691b8500fe5824bc2243c76a81
pekgnee <pekgnee@hotmail.com>
2019-04-05 01:22:08 +0800

    Update actions.py

--- actions.py
+++ actions.py
@@ -17,12 +17,13 @@ class ActionSearchAttraction(Action):
    def run(self, dispatcher, tracker, domain):
       # type: (CollectingDispatcher, Tracker, Dict[Text, Any]) -> List[Dict[Text, Any]]
 
-      url_params = {'apikey': apikey}
-      url_path = urljoin('https://tih-api.stb.gov.sg/content/v1/attractions/name/', attraction)
+      keyword_url = urllib.parse.quote(attraction)
+      url_params = {'apikey': apikey, 'keyword': keyword_url}
+      url_path = 'https://tih-api.stb.gov.sg/content/v1/attractions/search'
       r = requests.get(url_path, params=url_params)
       
       # Check if r.status_code successful
       json_data = json.loads(r.text)      
-      result = json_data["data"]["admissionInfo"]
-
-      return [SlotSet("admission", result if result is not None else [])]
+      data_list = json_data['data']
+      result = next((data_item for data_item in data_list if keyword.lower() in data_item['name'].lower()), None)      
+      return [SlotSet("search_result", result if result is not None else [])]
